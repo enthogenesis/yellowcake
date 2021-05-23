2b97b7f624c861214e9b3c0f01c2899dea4c4337
Matthew Thistle <enthogenesis@gmail.com>
2021-05-23 18:18:38 +0300

    today's edits bungling around lets see it I can see the changes to the netlifycms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <Layout>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+      </Layout>
+  )
 }
